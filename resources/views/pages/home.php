48bbddf2e611aae624f9d0ea0c42806c33ab5718
matteobnvl <matteobonneval19@gmail.com>
2022-06-17 14:02:30 +0200

    responsive

--- resources/views/pages/home.php
+++ resources/views/pages/home.php
@@ -120,7 +120,6 @@ const TLLetters = gsap.timeline();
 TLLetters
 .from(ligne, {width: '0%', opacity: 0, duration: 0.7, ease: "power1.in"})
 .from(allLetters, {opacity:0, stagger: 0.1, ease: "power2.out"})
-//.set(ligne, {width: '85vw'})
 
 
 
